69d8ad4ede5b57d50fae46c0b8a8eb970788d46b
Diana Khizhnova <khnizovad@gmail.com>
2025-12-04 12:04:24 +0500

    feat: init project

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+const handle = () => {
+  console.log('click')
+}
+</script>
+
+<template>
+  <HelloWorld message="Hello world" />
+  <button class="btn" @click="handle">start</button>
+</template>
+
+<style scoped>
+.btn {
+  width: 200px;
+  height: 100px;
+  background-color: blue;
+  cursor: pointer;
+
+  &:hover {
+    background-color: yellow;
+  }
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  message: string
+}
+
+defineProps<Props>()
+
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+
+</style>
